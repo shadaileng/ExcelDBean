3c76fa19173aabb7256107b5cad518669858b292
shadaileng <qpf0510@qq.com>
2018-04-25 17:28:49 +0800

    完成生成实体类文本基本功能

--- JavaCode/src/main/java/com/qpf/code/JavaCode.java
+++ JavaCode/src/main/java/com/qpf/code/JavaCode.java
@@ -18,9 +18,12 @@ public class JavaCode {
 	private boolean isInterface;
 	private List<String> interfaces;
 	private String subClassName;
-	private List<String> fields;
+	private List<Field> fields;
 	private Map<String, String> methods;
 	private static Logger logger = Logger.getLogger(JavaCode.class);
+	private int indent;
+//	private final String NEWLINE = "\n";
+	private final String NEWLINE = System.lineSeparator();
 	public JavaCode() {
 		packageName = "";
 		importPackage = new ArrayList<String>();
@@ -30,8 +33,9 @@ public class JavaCode {
 		subClassName = "";
 		isInterface = false;
 		interfaces = new ArrayList<String>();
-		fields = new ArrayList<String>();
+		fields = new ArrayList<Field>();
 		methods = new HashMap<String, String>();
+		indent = 0;
 	}
 	public JavaCode(String className, boolean isInterface, String desc){
 		this();
@@ -59,6 +63,21 @@ public class JavaCode {
 		}
 	}
 	
+	/**
+	 * 添加注解
+	 * @param annotation
+	 */
+	public void addAnnotation(String annotation){
+		if(isNotEmpty(annotation)){
+			importPackage(annotation);
+			annotations.add(annotation);
+		}
+	}
+	
+	/**
+	 * 继承父类
+	 * @param subClassName
+	 */
 	public void extendsSubClass(String subClassName){
 		logger.info("extends: " + subClassName);
 		if(isNotEmpty(subClassName)){
@@ -67,6 +86,10 @@ public class JavaCode {
 		}
 	}
 	
+	/**
+	 * 实现接口
+	 * @param interfaceName
+	 */
 	public void implementInterface(String interfaceName){
 		logger.info("implement: " + interfaceName);
 		if(isNotEmpty(interfaceName)){
@@ -82,21 +105,14 @@ public class JavaCode {
 	 * @param type  属性类型
 	 * @param init  初始值
 	 */
-	public void addFields(String modify, String field, String type, String init){
-		logger.info("add field: " + type + " " + field + " = " + init);
-		if(isNotEmpty(field) && isNotEmpty(type)){
+	public void addFields(String modify, String type, String fieldName, String init){
+		logger.info("add field: " + type + " " + fieldName + " = " + init);
+		if(isNotEmpty(fieldName) && isNotEmpty(type)){
 			importPackage(type);
-			new Field(modify, type, field, init);
-			
-			
-			StringBuffer buf = new StringBuffer("private ");
-			buf.append(splitFullClassName(type)[1] + " ");
-			buf.append(field + " ");
-			if(isNotEmpty(init)){
-				buf.append("= " + init);
-			}
-			buf.append(";");
-			this.fields.add(buf.toString());
+			Field field = new Field(modify, type, fieldName, init);
+			String result = field.getResult();
+			logger.info("field: " + result);
+			this.fields.add(field);
 		}
 	}
 	
@@ -109,15 +125,125 @@ public class JavaCode {
 		this.methods.put(methodDefine, body);
 	}
 	
-
+	/**
+	 * 添加构造函数
+	 */
+	public void addConstructor(){
+		String methodDefine = "public " + className + " ()";
+		String methodBody = "{" + newLine(1) + "super();" + newLine(-1) + "}" + newLine(0);
+		addmethod(methodDefine, methodBody);
+		
+		methodDefine = "public " + className + " (";
+		StringBuffer buf = new StringBuffer();
+		for(Field field : fields){
+			buf.append(field.getType() + " " + field.getFieldName() + ", ");
+		}
+		methodDefine += buf.toString();
+		methodDefine = methodDefine.substring(0, methodDefine.lastIndexOf(","));
+		methodDefine += ")";
+		methodBody = "{" + newLine(1);
+		buf = new StringBuffer();
+		for(Field field : fields){
+			buf.append("this." + field.getFieldName() + " = " + field.getFieldName() + ";" + newLine(0));
+		}
+		methodBody += buf.toString() + newLine(-1) + "}" + newLine(0);
+		addmethod(methodDefine, methodBody);
+	}
 	
-	
-	public void addConstructor(Map<String, String> params){
-		String methodDefine = "public " + className + " (";
-		for(Map.Entry<String, String> entry : params.entrySet()){
-//			entry.getKey() + 
+	/**
+	 * 添加getter和setter
+	 */
+	public void addGetterAndSetter(){
+		for(Field field : fields){
+			String name = field.getFieldName();
+			String type = field.getType();
+			String methodDefine = "";
+			String methodBody = "";
+			if(name.indexOf("is") == 0){
+				methodDefine = "public " + type + name + " ()";
+				methodBody = "{" + newLine(1) + "return this." + name + newLine(-1) + "}" + newLine(0);
+				addmethod(methodDefine, methodBody);
+				methodDefine = "public " + type + " set" + name.substring(2) + " (" + type + " " + name + ")";
+				methodBody = "{" + newLine(1) + "this." + name + " = " + name + ";" + newLine(-1) + "}" + newLine(0);
+			}else{
+				methodDefine = "public " + type + " get" + firstUpper(name) + " ()";
+				methodBody = "{" + newLine(1) + "return this." + name + newLine(-1) + "}" + newLine(0);
+				addmethod(methodDefine, methodBody);
+				methodDefine = "public " + type + " set" + firstUpper(name) + " (" + type + " " + name + ")";
+				methodBody = "{" + newLine(1) + "this." + name + " = " + name + ";" + newLine(-1) + "}" + newLine(0);
+			}
+			if(!"".equals(methodDefine)){
+				methods.put(methodDefine, methodBody);
+			}
+			
 		}
 	}
+	
+	/**
+	 * 构建实体类文本
+	 */
+	public void build(){
+		StringBuffer buf = new StringBuffer();
+		// 包名
+		if(isNotEmpty(this.packageName)){
+			buf.append("package " + packageName + ";" + newLine(0));
+		}
+		// 导入包
+		for(String packages : importPackage){
+			buf.append("import " + packages + ";" + newLine(0));
+		}
+		// 类注释
+		buf.append("/**" + newLine(0) + " * " + desc4Class + newLine(0) + " **/" + newLine(0));
+		
+		// 类注解
+		for(String str : annotations){
+			buf.append("@" + str + newLine(0));
+		}
+		
+		// 类声明
+		if(!isInterface){
+			buf.append("public class " + className);
+		}else{
+			buf.append("public interface " + className);
+		}
+		// 继承
+		if(!"".equals(subClassName)){
+			buf.append(" extends " + subClassName);
+		}
+		// 接口
+		if(interfaces != null && interfaces.size() > 0){
+			buf.append(" implement ");
+			String tmp = "";
+			for(String str : interfaces){
+				tmp += str + ", ";
+			}
+			tmp.substring(0, tmp.lastIndexOf(","));
+			buf.append(tmp);
+		}
+		buf.append(" {" + newLine(1));
+		
+		// 定义属性
+		for(Field field : fields){
+			buf.append(field.getResult() + newLine(0));
+		}
+		
+		// 构造函数
+		addConstructor();
+		
+		//getter & setter
+		addGetterAndSetter();
+		
+		for(Map.Entry<String, String> entry : methods.entrySet()){
+			buf.append(entry.getKey() + entry.getValue());
+		}
+		
+		
+		
+		buf.append(newLine(-1) + "}");
+		
+		System.out.println(buf.toString());
+	}
+	
 	
 	public String[] splitFullClassName(String className){
 		if(isNotEmpty(className)){
@@ -134,13 +260,25 @@ public class JavaCode {
 		return str != null && str.length() > 0;
 	}
 	
+	public String newLine(int flag){
+		indent += flag;
+		String newline = NEWLINE;
+		for(int i = 0; i < indent; i++){
+			newline += "\t";
+		}
+		System.out.println(indent);
+		return newline;
+	}
 	
+	public String firstUpper(String fieldName){
+		return fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+	}
 	@Override
 	public String toString() {
 		return "JavaCode [packageName=" + packageName + ", importPackage=" + importPackage + ", desc4Class="
 				+ desc4Class + ", annotations=" + annotations + ", className=" + className + ", isInterface="
-				+ isInterface + ", fields=" + fields + ", methods=" + methods + "]";
+				+ isInterface + ", interfaces=" + interfaces + ", subClassName=" + subClassName + ", fields=" + fields
+				+ ", methods=" + methods + ", indent=" + indent + "]";
 	}
-	
 	
 }
--- JavaCode/src/test/java/com/qpf/code/JavaCodeTest.java
+++ JavaCode/src/test/java/com/qpf/code/JavaCodeTest.java
@@ -6,6 +6,9 @@ public class JavaCodeTest {
 	@Test
 	public void newTest(){
 		JavaCode javaCode = new JavaCode("com.qpf.domain.User", false, "用户");
+		javaCode.addFields("private", "String", "name", null);
 		System.out.println(javaCode);
+		javaCode.build();
+		
 	}
 }
